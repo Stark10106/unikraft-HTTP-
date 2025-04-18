48ca8aa38476d1ea80f5d64840b9b3393caf5f3d
Vedant Vakharia <intelligentvedant@gmail.com>
2025-04-18 19:19:00 +0530

    Update http_server.c

--- http_server.c
+++ http_server.c
@@ -19,6 +19,7 @@ static unsigned long request_count = 0;
 static struct timeval start_time;
 static int connection_errors = 0;
 
+__attribute__((always_inline))
 static inline void get_uptime(struct timeval *uptime) {
     struct timeval now;
     gettimeofday(&now, NULL);
@@ -30,8 +31,9 @@ static inline void get_uptime(struct timeval *uptime) {
     }
 }
 
+__attribute__((always_inline))
 static inline void parse_path(const char *req, char *path, size_t maxlen) {
-    path[0] = '/'; path[1] = '\0'; // default to "/"
+    path[0] = '/'; path[1] = '\0';
     if (memcmp(req, "GET ", 4) != 0) return;
 
     const char *p = req + 4;
@@ -45,61 +47,64 @@ static inline void parse_path(const char *req, char *path, size_t maxlen) {
     }
 }
 
+static const char favicon_response[] =
+    "HTTP/1.1 404 Not Found\r\n"
+    "Content-Length: 0\r\n"
+    "Connection: close\r\n\r\n";
+
+static const char html_body_template[] =
+    "<html><body>"
+    "<h1>Unikraft HTTP Server</h1>"
+    "<p>This is a unikernel running a minimal HTTP server!</p>"
+    "<h2>Server Statistics:</h2>"
+    "<ul>"
+    "<li>Uptime: %ld.%06ld seconds</li>"
+    "<li>Requests: %lu</li>"
+    "<li>Errors: %d</li>"
+    "<li>Path: %s</li>"
+    "</ul>"
+    "<p>URLs: <a href=\"/\">Home</a> | <a href=\"/stats\">Stats</a></p>"
+    "</body></html>";
+
+__attribute__((always_inline))
 static inline void generate_response(char *buf, size_t *out_len, const char *path) {
     struct timeval uptime;
     get_uptime(&uptime);
 
     char *p = buf;
 
-    if (strcmp(path, "/favicon.ico") == 0) {
-        p += sprintf(p,
-            "HTTP/1.1 404 Not Found\r\n"
-            "Content-Length: 0\r\n"
-            "Connection: close\r\n\r\n");
-        *out_len = p - buf;
+    if (__builtin_expect(strcmp(path, "/favicon.ico") == 0, 0)) {
+        memcpy(buf, favicon_response, sizeof(favicon_response) - 1);
+        *out_len = sizeof(favicon_response) - 1;
         return;
     }
 
-    if (strcmp(path, "/stats") == 0) {
+    if (__builtin_expect(strcmp(path, "/stats") == 0, 0)) {
         p += sprintf(p,
             "HTTP/1.1 200 OK\r\n"
             "Content-Type: application/json\r\n"
-            "Connection: close\r\n");
+            "Content-Length: ");
+
+        char *body_start = p;
+        p += sprintf(p, "\r\nConnection: close\r\n\r\n");
 
         int header_len = p - buf;
-        p += sprintf(p,
-            "\r\n"
+        int json_len = sprintf(p,
             "{\n"
             "  \"uptime\": %ld.%06ld,\n"
             "  \"requests\": %lu,\n"
             "  \"errors\": %d\n"
             "}\n",
             uptime.tv_sec, uptime.tv_usec, request_count, connection_errors);
-        
-        size_t body_len = (p - buf) - header_len;
-        memmove(buf + header_len - 2, buf + header_len, body_len);  // remove extra \r\n
-        p = buf + header_len - 2 + body_len;
+
+        sprintf(body_start - 2, "%d", json_len); // backfill Content-Length
+        p += json_len;
         *out_len = p - buf;
         return;
     }
 
-    // default: HTML
-    const char *html_body_fmt =
-        "<html><body>"
-        "<h1>Unikraft HTTP Server</h1>"
-        "<p>This is a unikernel running a minimal HTTP server!</p>"
-        "<h2>Server Statistics:</h2>"
-        "<ul>"
-        "<li>Uptime: %ld.%06ld seconds</li>"
-        "<li>Requests: %lu</li>"
-        "<li>Errors: %d</li>"
-        "<li>Path: %s</li>"
-        "</ul>"
-        "<p>URLs: <a href=\"/\">Home</a> | <a href=\"/stats\">Stats</a></p>"
-        "</body></html>";
-
     char *html_start = p + 512;
-    int html_len = sprintf(html_start, html_body_fmt,
+    int html_len = sprintf(html_start, html_body_template,
         uptime.tv_sec, uptime.tv_usec, request_count, connection_errors, path);
 
     p += sprintf(p,
@@ -108,15 +113,14 @@ static inline void generate_response(char *buf, size_t *out_len, const char *pat
         "Content-Length: %d\r\n"
         "Connection: close\r\n\r\n",
         html_len);
-    
+
     memcpy(p, html_start, html_len);
     p += html_len;
-
     *out_len = p - buf;
 }
 
 int main(void) {
-    int srv, client, rc;
+    int srv, client;
     ssize_t n;
     struct sockaddr_in srv_addr;
     char response[BUFLEN];
@@ -124,7 +128,6 @@ int main(void) {
     char path[128];
 
     gettimeofday(&start_time, NULL);
-    printf("Minimal HTTP unikernel server starting...\n");
 
     srv = socket(AF_INET, SOCK_STREAM, 0);
     if (srv < 0) return 1;
@@ -138,30 +141,31 @@ int main(void) {
 
     if (bind(srv, (struct sockaddr *)&srv_addr, sizeof(srv_addr)) < 0)
         return 1;
-    if (listen(srv, 1) < 0) return 1;
+    if (listen(srv, 16) < 0) return 1;
 
-    printf("Listening on port %d...\n", LISTEN_PORT);
-
-    while (1) {
+    for (;;) {
         client = accept(srv, NULL, 0);
         if (client < 0) {
             connection_errors++;
             continue;
         }
 
-        // Disable Nagle
         int flag = 1;
         setsockopt(client, IPPROTO_TCP, TCP_NODELAY, &flag, sizeof(flag));
 
-        memset(recvbuf, 0, BUFLEN);
-        read(client, recvbuf, BUFLEN);
+        n = read(client, recvbuf, BUFLEN);
+        if (n <= 0) {
+            connection_errors++;
+            close(client);
+            continue;
+        }
 
         request_count++;
         parse_path(recvbuf, path, sizeof(path));
         generate_response(response, &response_len, path);
-        
-        n = send(client, response, response_len, 0);
-        if (n < 0) connection_errors++;
+
+        if (send(client, response, response_len, 0) < 0)
+            connection_errors++;
 
         close(client);
     }
